37a6f850e1e3be45472ae62b23776d48093b3ee8
Alexandre Cougard <alexandre.cougard@mahi-mahi.fr>
2013-08-23 12:59:36 +0200

    Fonctionnement show / hide google maps ok sur ie8

--- pages/page4.php
+++ pages/page4.php
@@ -1,10 +1,20 @@
-<div id='page4'>
+<div id="page4">
 	<img src="img/takeAway.png" id="takeAway" class="animate hires" alt="Take Away"/>	
 	<a target="_blank" href="http://www.facebook.com/pages/Ask-Media/425154660874487?ref=stream"><img src="img/Facebook-icon.png" id="contactIcon2" class="animate hires" alt="Facebook"/></a>
-	<div id="bgMaskMaps" onclick="$(this).fadeOut(300);"><div id="MapFrame"><iframe id="MapZone" style="border-radius:8px;" width="795px" height="595px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.fr/maps?t=m&amp;cid=0x28294f6e3357d44e&amp;dg=opt&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=48.894558,2.3488&amp;spn=0.009312,0.018882&amp;iwloc=A&amp;output=embed"></iframe></div></div>
-	<a onClick="$('#bgMaskMaps').fadeIn(300);" target="_blank" style="cursor:hand;background-color:white;"><img src="img/GMaps-icon.png" id="contactIcon1" class="animate hires" alt="Google Maps"/></a>
-	<a target="_blank" href="mailto:contact@askmedia.fr" type="submit"><img src="img/Mail-Icon.png" id="contactIcon4" class="animate hires" alt="Mail"/></a>
-	<a target="_blank" href="https://twitter.com/AskMedia_"><img src="img/Twitter-Icon.png" id="contactIcon3" class="animate hires" alt="Twitter"/></a>
+	<div id="bgMaskMaps" class="visuallyhidden" onclick="$(this).addClass('visuallyhidden').fadeOut(300);">
+		<div id="MapFrame">
+			<iframe id="MapZone" style="border-radius:8px;" width="795px" height="595px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.fr/maps?t=m&amp;cid=0x28294f6e3357d44e&amp;dg=opt&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=48.894558,2.3488&amp;spn=0.009312,0.018882&amp;iwloc=A&amp;output=embed"></iframe>
+		</div>
+	</div>
+	<a onClick="$('#bgMaskMaps').removeClass('visuallyhidden').fadeIn(300);" target="_blank" style="cursor: pointer;background-color:white;">
+		<img src="img/GMaps-icon.png" id="contactIcon1" class="animate hires" alt="Google Maps"/>
+	</a>
+	<a target="_blank" href="mailto:contact@askmedia.fr" type="submit">
+		<img src="img/Mail-Icon.png" id="contactIcon4" class="animate hires" alt="Mail"/>
+	</a>
+	<a target="_blank" href="https://twitter.com/AskMedia_">
+		<img src="img/Twitter-Icon.png" id="contactIcon3" class="animate hires" alt="Twitter"/>
+	</a>
 	<img src="img/Ellipse3.png" id="contact-ellipse" class="animate hires" alt="Ellipse3"/>
 	<img src="img/ill4.png" id="ill4" class="animate hires" alt=""/>
 </div>
